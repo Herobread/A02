a5381c3c7e603c1d78b1b6e296e6e7d6955ab36a
Alex Nazarenko <alexeybread81@gmail.com>
2023-10-07 21:15:31 +0100

    add logs

--- fetch-data.sh
+++ fetch-data.sh
@@ -25,26 +25,28 @@ if [ ! -d "$DIRECTORY" ]; then
 fi
 
 cd "$DIRECTORY" || exit 1
+echo "[Info] $(date)" > log.txt
+echo "[Info] Downloading data from $FILE_LIST_URL into $DIRECTORY" >> log.txt
 
 # Check if filelist.txt already exists
 if [ -f "filelist.txt" ]; then
-  read -p "The file 'filelist.txt' already exists. Do you want to overwrite it? (Y/n): " response
+  read -p "[?] The file 'filelist.txt' already exists. Do you want to overwrite it? (Y/n): " response
   if [[ ! "$response" =~ ^[Yy]$ ]]; then
-    echo "Download aborted."
+    echo "[Info] Download aborted: 'filelist.txt' already exists." | tee -a log.txt
     exit 0
   fi
 fi
 
 # Create the 'data' and 'out' subdirectories or clear their contents
 if [ -d "data" ]; then
-  echo "Clearing contents of 'data' directory..."
+  echo "[Info] Clearing contents of 'data' directory..." | tee -a log.txt
   rm -rf data/*
 else
   mkdir data
 fi
 
 if [ -d "out" ]; then
-  echo "Clearing contents of 'out' directory..."
+  echo "[Info] Clearing contents of 'out' directory..." | tee -a log.txt
   rm -rf out/*
 else
   mkdir out
@@ -52,7 +54,7 @@ fi
 
 # Download the filelist.txt from the FILE_LIST_URL
 if ! wget "$FILE_LIST_URL"; then
-  echo "Error: Failed to download 'filelist.txt' from '$FILE_LIST_URL'" >&2
+  echo "[Error] Failed to download 'filelist.txt' from '$FILE_LIST_URL'" &>> log.txt
   exit 1
 fi
 
@@ -62,12 +64,18 @@ while IFS= read -r file; do
   encoded_file=$(echo "$file" | sed 's/ /%20/g')
   
   # Construct the full URL for the file
-  full_file_url="${BASE_URL}${encoded_file}"
+  FULL_FILE_URL="${BASE_URL}${encoded_file}"
 
-  # Download the file to the 'data' directory
-  if ! wget "$full_file_url" -P data/; then
-    echo "Error: Failed to download '$file' from '$full_file_url'" >&2
+  echo "[Info] Downloading file from $FULL_FILE_URL" >> log.txt
+  if ! wget "$FULL_FILE_URL" -P data/; then
+    echo "[Error] Failed to download '$file' from '$FULL_FILE_URL'" &>> log.txt
+    continue
   fi
+  echo "[Info] Downloaded file from $FULL_FILE_URL" >> log.txt
+
 done < filelist.txt
+
+echo "[Info] Program finished" >> log.txt
+echo "[Info] $(date)" >> log.txt
 
 rm -f filelist.txt
